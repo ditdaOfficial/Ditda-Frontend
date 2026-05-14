c35e4bb34915ed1d5c8212bbd0abf49a22c611af
YuminPark <p353700@gmail.com>
2026-05-14 18:42:25 +0900

    [SETTING] Next.js + TypeScript + TailwindCSS 초기세팅

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  nextTs,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "prettier/prettier": "error",
+    },
+  },
+  prettierConfig,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: [{ loader: "@svgr/webpack", options: { dimensions: false } }],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
