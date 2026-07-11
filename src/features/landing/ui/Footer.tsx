8af3ebfd764da634962741ccf39459ea5495f112
ditdaOfficial <ditda.official@gmail.com>
2026-07-11 06:23:27 +0000

    #59 [CHORE] 서비스 소개서 다운로드 기능 구현

--- src/features/landing/ui/Footer.tsx
+++ src/features/landing/ui/Footer.tsx
@@ -1,6 +1,13 @@
 import { WhiteLogo } from "@/shared/assets/logos";
 import Button from "@/shared/ui/Button";
 
+const handleDownload = (fileUrl: string, fileName: string) => {
+  const link = document.createElement("a");
+  link.href = fileUrl;
+  link.download = fileName;
+  link.click();
+};
+
 const Footer = () => {
   return (
     <footer className="bg-gray-80 flex items-center justify-between px-40 py-12">
@@ -22,8 +29,28 @@ const Footer = () => {
         </div>
         <div className="flex w-61.5 shrink-0 flex-col gap-1 whitespace-nowrap">
           <p className="text-gray-40 text-body2-m">서비스 소개서</p>
-          <Button variant="medium_secondary">강사용 서비스 소개서 다운로드</Button>
-          <Button variant="medium_secondary">디자이너용 서비스 소개서 다운로드</Button>
+          <Button
+            variant="medium_secondary"
+            onClick={() =>
+              handleDownload(
+                "/files/service_intro_instructor.pdf",
+                "ditda_강사용_서비스_소개서.pdf",
+              )
+            }
+          >
+            강사용 서비스 소개서 다운로드
+          </Button>
+          <Button
+            variant="medium_secondary"
+            onClick={() =>
+              handleDownload(
+                "/files/service_intro_designer.pdf",
+                "ditda_디자이너용_서비스_소개서.pdf",
+              )
+            }
+          >
+            디자이너용 서비스 소개서 다운로드
+          </Button>
         </div>
       </div>
     </footer>
